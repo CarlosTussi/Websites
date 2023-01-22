dc4374c2c9de5fc9c446746b18d90b2249fb6ef7
Carlos Eduardo Tussi <cetleite@gmail.com>
2023-01-22 13:16:23 +0100

    [ADD] Opengraph protocol

--- Carlostussi/carlostussi/src/components/Footer.js
+++ Carlostussi/carlostussi/src/components/Footer.js
@@ -1,6 +1,7 @@
 import { HStack, VStack } from "@chakra-ui/react";
 import * as GUIDE from "../styleguide/colorscheme";
-import { Linkedin, GitHub } from 'react-feather';
+import { Linkedin, GitHub, Hexagon } from 'react-feather';
+
 
 function Footer()
 {
@@ -12,7 +13,7 @@ function Footer()
         }}>
             <VStack bg={GUIDE.color.secondary}
                     p="3vh 0 3vh 0">
-                <p>Logo</p>
+                <Hexagon size="5vh"/>
                 <HStack spacing="10vw" p="2vh 0 2vh 0">
                     <a href="https://www.linkedin.com/in/cetleite/" target="_blank" rel="noreferrer"><Linkedin className="socialIcon" strokeWidth="0.5px" size="6vh"/></a>
                     <a href="https://github.com/CarlosTussi/" target="_blank" rel="noreferrer"><GitHub className="socialIcon" strokeWidth="0.5px" size="6vh"/></a>
