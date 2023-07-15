943ad66e3320a0556a4001e7653423ee249ad158
Carlos Eduardo Tussi <cetleite@gmail.com>
2023-07-15 16:16:34 +0200

    [ADD] Desktop front-end veersion of authentication page

--- authentication/src/pages/Login.js
+++ authentication/src/pages/Login.js
@@ -1,18 +1,29 @@
 import LoginForm from "../components/LoginForm";
 import { Linkedin, Facebook, Aperture } from "react-feather";
+import src_logo  from '../assets/github.png';
 
 function Login()
 {
     return(
-        <div>
-            <div className="login-header">
-            <Aperture color="#35ADA1" size="2em"/>  
-                <div >
-                    <h1>Login Now</h1>
-                    <h2>Please enter your details</h2>
-                </div> 
-            </div>                   
-            <LoginForm />
+        <div className="login-page">
+            <div className="desktop-login-page">
+                <div className="login-header">
+                    {/* <Aperture color="#35ADA1" size="2em"/>   */}
+                    <img src={src_logo} alt="Website logo"/>
+                    <div className="login-header-mobile">
+                        <h1>Login Now</h1>
+                        <h2>Please enter your details</h2>
+                    </div> 
+                </div>
+                <div className="desktop-leftside-container">
+                    <div className="login-header-desktop">
+                        <h1>Login Now</h1>
+                        <h2>Please enter your details</h2>
+                    </div> 
+                    <LoginForm />
+                </div>                   
+                
+            </div>
             <div className="login-footer-container">
                 <div className="footer-content">
                     <div className="footer-social-icon">
