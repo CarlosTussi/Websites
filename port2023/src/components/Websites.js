230deb4b2b71af9f30f4d5f158afdbb20fc10fc7
Carlos Eduardo Tussi <cetleite@gmail.com>
2023-08-12 20:19:17 +0200

    [ADD] Work page version 1

--- port2023/src/components/Websites.js
+++ port2023/src/components/Websites.js
@@ -10,15 +10,29 @@ import 'swiper/css/pagination';
 import 'swiper/modules';
 import { Fade } from "react-awesome-reveal";
 
+import fashion_src from '../assets/fashion.jpg'
+import auth_src from '../assets/auth.jpg'
+
 function Websites()
 {
     const projects = [1,2,3,4,5];
     return(
         <div className="websites">
             <Fade duration="1000">
-                <WorkCard />
-                <WorkCard />
-                <WorkCard />
+                <WorkCard imgsrc={fashion_src} 
+                          name="Fashion Landing Page"
+                          description="Meticulously crafted fashion design website's landing page. The landing page serves as the digital red carpet, enticing visitors with a seamless blend of aesthetics and functionality."
+                          link="https://fashion-a9e4f.web.app"
+                          alt="Home screen of a fashion webpage showing a navigation menu and two pursers as a fashion item"
+                          />
+                <WorkCard imgsrc={auth_src} 
+                          name="Authentication Page"
+                          description="Indulge in an immersive experience with our responsive authentication page, expertly curated with a delicate equilibrium of accent and primary colors. The visual symphony of hues not only beckons users but also sets the stage for a seamless login journey."
+                          link="https://authentication-e1bcb.web.app/"
+                          alt="Authentication page. Containing a login form, submit button and a logo."
+                          />
+                {/* <WorkCard /> */}
+                {/* <WorkCard /> */}
             </Fade>
 
 
--- port2023/src/components/WorkCard.js
+++ port2023/src/components/WorkCard.js
@@ -1,12 +1,14 @@
-function WorkCard()
+function WorkCard(props)
 {
     return(
-        <div className="work-card">            
-            <div className="card-image"></div>
+        <div className="work-card">                        
+            <div className="img-container">
+                <img src={props.imgsrc} alt={props.alt}/>                
+            </div>
             <div className="card-content">
-                <h5>The Cocktail Guide</h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam beatae quae vitae facere, ad sint maiores aut sit ullam consectetur perspiciatis repellat quasi quos ipsam in exercitationem illo, laboriosam recusandae?</p>             
-                <div><a href="">See More</a></div>
+                <h5>{props.name}</h5>
+                <p>{props.description}</p>
+                <div className="live-link-container"><a href={props.link} target="_blank" rel="noreferrer">See it Live</a></div>
                 
             </div>
            
