9abdf8babc360acbfa5cd254e0029ec31dd2c06c
Carlos Eduardo Tussi <cetleite@gmail.com>
2023-11-28 23:28:09 +0100

    [ADD] Draft for the About page

--- cocktailguide/src/pages/About.js
+++ cocktailguide/src/pages/About.js
@@ -2,7 +2,37 @@ function About ()
 {
     return (
         <section className="about-page">
-            <h1>Coming soon</h1>
+            <h1>Coming Soon</h1>
+            {/* <h1>About Us</h1>
+            <p>
+            Welcome to The Cocktail Guide – Your Passport to Mixology Excellence!
+            🍹 Elevate your mixology game with our meticulously curated cocktail 
+            guide, where artistry meets refreshment. At The Cocktail Guide, 
+            we are passionate about crafting an immersive experience that 
+            not only quenches your thirst but also stimulates your senses.
+            </p>
+
+            <h2>Our Mission</h2>
+            <p>
+            At The Cocktail Guide, our mission is to bring the world of mixology 
+            to your fingertips. Whether you're a seasoned bartender or a home 
+            enthusiast, we're here to inspire and guide you through the exciting 
+            realm of cocktails. Our team of cocktail connoisseurs is dedicated 
+            to delivering a diverse array of recipes, tips, and techniques that 
+            cater to all taste preferences.
+            </p>
+            <h2>What Sets us Apart</h2>
+            <p>
+            What Sets Us Apart
+                1. Exquisite Recipes:
+                Explore a treasure trove of handpicked cocktail recipes ranging from timeless classics to innovative concoctions. Each recipe is accompanied by detailed instructions and vibrant visuals to ensure your mixology journey is as enjoyable as the final sip.
+
+                2. Responsive Design:
+                Our website is designed with you in mind. Whether you're browsing on a desktop, tablet, or smartphone, [Your Cocktail Guide] ensures a seamless and visually stunning experience. We believe that great design enhances the enjoyment of great cocktails.
+
+                3. Educational Content:
+                Dive deep into the world of mixology with our educational articles, guiding you through the history of cocktails, the art of garnishing, and the science behind the perfect pour. We believe that understanding the fundamentals enhances the appreciation of every sip.
+            </p> */}
         </section>
     )
 }
